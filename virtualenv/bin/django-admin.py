b00942e2763b19f0b5720d781d23e1d1de56ec88
zaidanali028 <zaidanali028@gmail.com>
2024-11-13 21:02:42 +0000

    Remove virtualenv folder from repository and add to .gitignore

--- virtualenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/ali/Desktop/ci-cd-1/semaphore-demo-python-django/virtualenv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
