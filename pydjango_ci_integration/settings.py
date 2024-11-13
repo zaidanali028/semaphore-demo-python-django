f5bf3f29c864aa91ad65e9e340efeb2db41d79e5
zaidanali028 <zaidanali028@gmail.com>
2024-11-13 21:29:42 +0000

    added semaphore

--- pydjango_ci_integration/settings.py
+++ pydjango_ci_integration/settings.py
@@ -90,11 +90,12 @@ DATABASES = {
         'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.mysql'),
         'NAME': os.getenv('DB_NAME', 'pydjango'),
         'USER': os.getenv('DB_USER', 'root'),
-        'PASSWORD': os.getenv('DB_PASSWORD', 'password'),
+        'PASSWORD': os.getenv('DB_PASSWORD', ''),
         'HOST': os.getenv('DB_HOST', '127.0.0.1'),
         'PORT': os.getenv('DB_PORT', '3306')
     }
 }
+
 
 
 # Password validation
